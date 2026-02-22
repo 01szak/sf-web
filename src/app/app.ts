422f33e3a63ff5aa7975cda4aff203640b23ed1c
KacperOlszewski <kacperolszewski2006@gmail.com>
2026-02-23 00:59:24 +0100

    add home page / main photo / collage wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './navbar/navbar';
+import {HomePage} from './home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, HomePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
